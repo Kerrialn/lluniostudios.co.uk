8b6be61c7738429b8021bb954fcdfdfd1ceb90fa
kez <kerrialbeckettnewham@gmail.com>
2025-06-01 18:32:35 +0200

    fix asset mapper

--- config/packages/asset_mapper.php
+++ config/packages/asset_mapper.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+
     $containerConfigurator->extension('framework', [
         'asset_mapper' => [
+            // @phpstan-ignore-next-line
             'server' => '%env(APP_ENV)%' !== 'prod',
             'paths' => [
                 'assets/',
