ad469d700353a526ee124e37056a823e50404377
kez <kerrialbeckettnewham@gmail.com>
2026-02-17 14:12:52 +0100

    fix translations

---
+++ translations/messages+intl-icu.en.php
@@ -0,0 +1,8 @@
+<?php
+
+return array (
+  'vich_uploader.form_label.delete_confirm' => 'Delete?',
+  'vich_uploader.link.download' => 'Download',
+  'buy-now' => 'buy now',
+  'request-access' => 'request access',
+);
